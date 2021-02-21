51a2f4c2dd4a21922a87afbddf78ac6ca49e5233
Maria Yakubova <mariaaliceyakubova@gmail.com>
2021-02-21 12:50:17 -0600

    Second commit

--- src/test/java/com/LibraryCT/pages/BookCategoriesPage.java
+++ src/test/java/com/LibraryCT/pages/BookCategoriesPage.java
@@ -1,6 +1,8 @@
 package com.LibraryCT.pages;
 
 import com.LibraryCT.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class BookCategoriesPage {
@@ -9,4 +11,13 @@ public class BookCategoriesPage {
 
         PageFactory.initElements(Driver.getDriver(),this);
     }
+    @FindBy(id ="book_categories")
+    public WebElement searchCategoriesBox;
+
+    @FindBy(xpath ="//select[@id=‘book_categories’]//option")
+    public WebElement dropdownCategories;
+
+
+
+
 }
